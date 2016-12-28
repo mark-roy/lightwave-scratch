705a9a44f085a0f060dfada8f25b93d7edd45bcb
mark-roy <mark-roy@users.noreply.github.com>
2016-12-28 16:25:31 -0500

    Update nonstop-explorer.js

--- nonstop-explorer.js
+++ nonstop-explorer.js
@@ -8,25 +8,38 @@
         return {status: 2, msg: 'Ready'};
     };
 
+    ext.jsonreference = function( json, reference ) {
+      return json[reference];
+    };   //  jsonreference
+    
+    var cpuStatus = {};
+    
     ext.getCpuStatus = function( callback ) {
-
       $.ajax({
           url: "http://lightwave-server.nuwavetech.io/explore/v1/cpu",
           success: function( json ) {
             console.log( "cpustatus=" + JSON.stringify( json ) );
-            callback( json );
+            cpuStatus = json;
+            callback( cpuStatus.cpuList.cpuCount );
           }   //  success
       });
-      
-    };
+    };   //  getCpuStatus
+
+    ext.getCpuBusy = function( processor ) {
+      if ( cpuStatus.cpuList && cpuStatus.cpuList.cpu.length < processor )
+        return cpuStatus.cpuList.cpu[processor].busy;
+      else
+        return 'error';
+    };   //  getCpuStatus
 
     // Block and block menu descriptions
     var descriptor = {
-        blocks: [
-            // Block type, block name, function name
-            ['R', 'Get CPU Status', 'getCpuStatus']
-        ],
-        url: 'http://www.nuwavetech.com/lightwave'
+      blocks: [
+        // Block type, block name, function name
+        [ 'R', 'Get CPU Count', 'getCpuStatus' ],
+        [ 'r', 'Get busy% for CPU %n', 'getCpuBusy', 0 ]
+      ],
+      url: 'https://mark-roy.github.io/lightwave-scratch/README.md'
     };
 
     // Register the extension
